3ab769d8e371095828798acb6a7f3040a84aad70
Interfish <330508653@qq.com>
2018-08-07 20:21:38 +0800

    Temp: tmp commit

--- public/source/js/cute_text.js
+++ public/source/js/cute_text.js
@@ -1,6 +1,28 @@
 $(document).ready(function () {
   $(document).on("zipper:end", function(){
     $(document).off('zipper:end');
-    console.log("hi");
+    var text = "打多久啊我看到健康拉丁舞";
+    function cute(i) {
+      if(i > text.length ) {
+        setTimeout(function(){
+          window.location.href = '/cake';
+        }, 5000)
+        return
+      }
+      if(i == text.length){
+        $(".cute").text('');
+      } else {
+        $(".cute").text(text[i]);
+      }
+      if(i > 0) {
+        $(".normal").text(text.slice(0, i));
+      }
+      setTimeout(function(){
+        cute(i+1);
+      }, 300);
+    }
+    setTimeout(function() {
+      cute(0);
+    }, 1500);
   });
 })
